d1e9d60e0d93e0f98fe7832c5da261ac595b99d4
liu-denny <2805852578@qq.com>
2019-08-01 10:32:29 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/chat/webController.java
@@ -0,0 +1,40 @@
+package com.example.demo.chat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author yudong
+ * @create 2019-07-31 17:19
+ */
+@Controller
+public class webController {
+    /**
+     * 登陆界面
+     */
+    @RequestMapping("/")
+    public ModelAndView login() {
+        return new ModelAndView("/login");
+    }
+
+    /**
+     * 聊天界面
+     */
+    @GetMapping("/index")
+    public ModelAndView index(String username, String password, HttpServletRequest request) throws UnknownHostException {
+        if (StringUtils.isEmpty(username)) {
+            username = "匿名用户";
+        }
+        ModelAndView mav = new ModelAndView("/chat");
+        mav.addObject("username", username);
+        mav.addObject("webSocketUrl", "ws://"+ InetAddress.getLocalHost().getHostAddress()+":"+request.getServerPort()+request.getContextPath()+"/chat");
+        return mav;
+    }
+}
